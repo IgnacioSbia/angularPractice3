1e34f519c5f88fe90b9dfcf1b03821af5b7bf2ae
Ignacio <ignaciosabiajes@gmail.com>
2023-10-21 13:42:38 -0300

    routing

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,18 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { AboutUsComponent } from './about-us/about-us.component';
+import { HomePageComponent } from './home-page/home-page.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    component: AboutUsComponent,
+    path:'aboutUs'
+ },
+ {
+  component: HomePageComponent,
+  path:''
+}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
