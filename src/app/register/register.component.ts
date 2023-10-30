86b6f27cc3f4031e8cca720c1b3de5931182c68d
Ignacio <ignaciosabiajes@gmail.com>
2023-10-30 10:45:48 -0300

    register end-point

--- src/app/register/register.component.ts
+++ src/app/register/register.component.ts
@@ -10,13 +10,34 @@ export class RegisterComponent {
   mail = "";
   password = "";
 
-  onKey(event: any){
-    this.password += event.target.value;
+  onKeyMail(event: any){
+    this.mail += event.target.value;
     console.log(this.mail)
   }
+  onKeyPassword(event: any){
+    this.password += event.target.value;
+    console.log(this.password)
+  }
   //Register Fetch
-   handleSubmit(){
-   
+   async handleSubmit(){
+    var myHeaders = new Headers();
+    myHeaders.append("Content-Type", "application/json");
+    const user = {
+      mail: this.mail,
+      password: this.password
+    };
+
+    var requestOptions:object = {
+      method: 'POST',
+      headers: myHeaders,
+      body: JSON.stringify(user),
+      redirect: 'follow'
+    };
+
+    await fetch("http://localhost:8000/api/register", requestOptions)
+      .then(response => response.text())
+      .then(result => console.log(result))
+      .catch(error => console.log('error', error));
   }
 
 }
